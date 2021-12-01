e9e84fe16da7c64e8fff055f65cc44d7cea34160
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-01 17:29:04 -0500

    testing not working currently

--- test/MusicList.test.js
+++ test/MusicList.test.js
@@ -26,4 +26,18 @@ describe('MusicList', () => {
     it('deploys a contract', () => {
         assert.ok(musicList.options.address);
     });
+
+    it('can add a song', async () => {
+        await musicList.methods.addSong('Lizzie McGuire', '3m french', 'youtube.com').send({ from: accounts[0] });
+
+        const numberOfSongs = await musicList.methods.getSongCount().call({ from: accounts[0] });
+        assert.equal('1', numberOfSongs);
+    });
+
+    // it('can remove a song', async () => {
+    //     await musicList.methods.addSong('Lizzie McGuire', '3m french', 'youtube.com').call();
+    //     const numberOfSongs = await musicList.methods.getSongCount().call();
+    //     console.log(numberOfSongs);
+    //     // await musicList.methods.removeSong('0').send({ from: accounts[0] });
+    // });
 });
