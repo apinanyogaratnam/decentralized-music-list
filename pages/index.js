ebeb14b64be9ca778f6fe9d3444f0cdd27fea373
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-01 17:28:56 -0500

    completed ui

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,17 @@ import musicList from '../ethereum/musicList';
 
 
 export default function Home() {
+  const [view , setView] = useState(false);
+
   const [songName, setSongName] = useState('');
   const [artistName, setArtistName] = useState('');
   const [url, setUrl] = useState('');
 
-  const [songCount, setSongCount] = useState('');
+  const [songCount, setSongCount] = useState(null);
   const [songList, setSongList] = useState([]);
 
-  const displaySongs = async () => {
-    const accounts = await web3.eth.getAccounts();
-    const numberOfSongs = await musicList.methods.getSongCount().call({ from: accounts[0] });
-    setSongCount(numberOfSongs);
-  }
-
-  useEffect(() => {
-    displaySongs();
-  });
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   const addSong = async (event) => {
     event.preventDefault();
@@ -31,17 +26,48 @@ export default function Home() {
     
     const txData = await musicList.methods.addSong(songName, artistName, url).send({ from: accounts[0] });
     console.log(txData);
+    setSongName('');
+    setArtistName('');
+    setUrl('');
+    viewSongs();
   };
 
   const removeSong = async (id) => {
     const accounts = await web3.eth.getAccounts();
     const txData = await musicList.methods.removeSong(id).send({ from: accounts[0] });
     console.log(txData);
-  }
+    viewSongs();
+  };
+
+  const getSongs = async () => {
+    const accounts = await web3.eth.getAccounts();
+    let numberOfSongs = await musicList.methods.getSongCount().call({ from: accounts[0] });
+    setSongCount(parseInt(numberOfSongs));
+    numberOfSongs = parseInt(numberOfSongs);
+
+    const songs = await Promise.all(
+      Array(numberOfSongs).fill().map((element, index) => {
+        return musicList.methods.getSong(index).call({ from: accounts[0] });
+      })
+    );
+    
+    setSongList(songs);
+  };
+
+  const viewSongs = () => {
+    setLoading(true);
+    setMessage('waiting for transaction');
+    getSongs();
+    setView(true);
+    setLoading(false);
+    setMessage('');
+  };
 
   return (
     <div>
-      <h1>{songCount}</h1>
+      {view ? null : <button onClick={viewSongs}>View your songs</button>}
+      {loading ? <p>{message}</p> : null}
+      {songCount === null ? null : <h1>{songCount}</h1>}
       <form>
         <input 
           type="text" 
@@ -70,18 +96,20 @@ export default function Home() {
       <div>
         <h1>Your Songs:</h1>
         {
-          songList.map((song, index) => {
+          songList.map((key, index) => {
             return (
-              <div>
-                <h2>{song.songName}</h2>
-                <h3>{song.artistName}</h3>
-                <h3>{song.url}</h3>
-                <button onClick={removeSong(index)}>Delete</button>
+              <div key={index}>
+                <h3>{key[0]}</h3>
+                <h3>{key[1]}</h3>
+                <h3>{key[2]}</h3>
+                <button onClick={() => removeSong(index)}>Delete</button>
               </div>
             );
           })
         }
       </div>
+      <p>Install metamask if you haven't done so already. Get eth from the faucet here: <a href="https://faucets.chain.link/rinkeby" target="_blank" rel="noopener">faucet</a></p>
+      <p>Make sure you are on the rinkeby testnet. Any funds sent through different networks will be lost.</p>
     </div>
   )
 }
