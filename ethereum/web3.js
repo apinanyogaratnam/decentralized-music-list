8e3aa5e7ee9f372f9babac9ca25ecb17d820abf5
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-01 12:08:50 -0500

    fixed typo

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
 } else {
     const provider = new Web3.providers.HttpProvider(
-        "https://rinkeby.infura.io/v3/d55c4f57b80445d284aa7754f355aaeb"3
+        "https://rinkeby.infura.io/v3/d55c4f57b80445d284aa7754f355aaeb"
     );
     web3 = new Web3(provider);
 }
